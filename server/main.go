f6d896dd2890df96b50c07c474966a2a4448f72b
Vitor Jordao <srvitorlol@gmail.com>
2020-06-29 10:19:34 -0300

    My first REST API

--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	print("Hello World!")
+
+	http.HandleFunc("/hello", HelloHandler)
+	http.ListenAndServe(":8887", nil)
+
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
 }
