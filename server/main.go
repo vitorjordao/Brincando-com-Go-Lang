05fcd2ecc4036a4caec665b9d0572c02e92822ee
Vitor Jordao <srvitorlol@gmail.com>
2020-06-29 10:49:47 -0300

    My first example on template engine

--- server/main.go
+++ server/main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+type Task struct {
+	Name string
+	Done bool
+}
 
 func main() {
 
 	http.HandleFunc("/hello", HelloHandler)
+	http.HandleFunc("/tasks", TasksHandler)
 	http.ListenAndServe(":8887", nil)
 
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
+}
+
+func TasksHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("server/tasks.html"))
+
+	tasks := []Task{
+		{"Tarefa 1", false},
+		{"Tarefa 2", true},
+	}
+
+	tmpl.Execute(w, tasks)
 }
